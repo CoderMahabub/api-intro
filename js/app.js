03bd35520b962612eb6f277249d66643cf475379
CoderMahabub <codermahabub@gmail.com>
2021-08-26 00:01:57 +0600

    32-2 Intro To Json, Json Structure, Parse, Stringify

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
+// JavaScript Object Notation
+//JSON
 const user = { id: 11, name: 'Gorib Amir', job: 'actor' };
-console.log('API');
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+
+const shop = {
+    name: 'Alia Store',
+    address: 'Ranbir Road',
+    profit: 15000,
+    products: ['Laptop', 'Mobile', 'Pepsi'],
+    owner: {
+        name: 'Alia Bhaat',
+        profession: 'Actor',
+    },
+    isExpensive: false
+};
+const shopStringified = JSON.stringify(shop);
+// console.log(shop);
+console.log(shopStringified);
+const coverted = JSON.parse(shopStringified);
+console.log(coverted);
