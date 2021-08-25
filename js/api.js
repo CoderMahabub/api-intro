1ba99c7d748c7f60caa8e79b4c13352ed1302571
CoderMahabub <codermahabub@gmail.com>
2021-08-26 01:26:52 +0600

    32-5 Dynamically Display Loaded Data On Your Website

--- js/api.js
+++ js/api.js
@@ -10,6 +10,16 @@ function loadUsers() {
         .then(data => displayUsers(data))
 }
 
+// Show in UI
+function displayUsers(data) {
+    const ul = document.getElementById('users');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = `Name: ${user.name}, Email ${user.email}`;
+        ul.appendChild(li);
+    }
+}
+
 function loadTodos() {
     fetch('https://jsonplaceholder.typicode.com/todos')
         .then(response => response.json())
@@ -19,9 +29,32 @@ function loadTodos() {
 function loadPosts() {
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
-        .then(res => console.log(res))
+        .then(data => displayPosts(data))
 }
 
-function displayUsers(data) {
-    console.log(data);
+function displayPosts(data) {
+    const ul = document.getElementById('users-post');
+    for (const post of data) {
+        const li = document.createElement('li');
+        li.innerText = `User ID: ${post.userId}, ID: ${post.id}, Title: ${post.title}, Message: ${post.body}`;
+        ul.appendChild(li);
+    }
+}
+
+//Load Photos from JSON
+function loadPhotos() {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(data => displayPhotos(data))
+}
+
+//Display Photo From JSON
+function displayPhotos(data) {
+    const loadPhoto = document.getElementById('load-photos');
+    for (const photo of data) {
+        // console.log(photo);
+        const article = document.createElement('article');
+        article.innerText = `Album ID: ${photo.id} | URL: ${photo.url} | Thumbnail: ${photo.thumbnail} | Album Title: ${photo.title} `;
+        loadPhoto.appendChild(article);
+    }
 }
