b0f3dc948bc8f7e4fe33d8cabc3900c0e95f4aa1
CoderMahabub <codermahabub@gmail.com>
2021-08-26 00:35:29 +0600

    32-4 Load More Data, More API, Send Data To Function

--- js/api.js
+++ js/api.js
@@ -4,3 +4,24 @@ function loadData() {
         .then(json => console.log(json));
 }
 
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayUsers(data))
+}
+
+function loadTodos() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(res => console.log(res))
+}
+
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(res => console.log(res))
+}
+
+function displayUsers(data) {
+    console.log(data);
+}
